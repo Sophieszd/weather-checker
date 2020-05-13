e9a7d84759b52bd3deb0695e3ad523f2f3b48ea7
Sophie <sophieszdixon@gmail.com>
2020-05-13 19:26:44 +0100

    Updates

---
+++ precache-manifest.9ddc543cbbab9a97eef84fa2610f2819.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c3d221a0122f46a4fcb10e5727f02c6",
+    "url": "/weather-checker/index.html"
+  },
+  {
+    "revision": "cb77cce0480a82aeddd6",
+    "url": "/weather-checker/static/css/main.5e5b9001.chunk.css"
+  },
+  {
+    "revision": "613887089eea6ad70a47",
+    "url": "/weather-checker/static/js/2.e1fdbf13.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-checker/static/js/2.e1fdbf13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb77cce0480a82aeddd6",
+    "url": "/weather-checker/static/js/main.9087b8e8.chunk.js"
+  },
+  {
+    "revision": "1f4378cdd5986bd2e2a0",
+    "url": "/weather-checker/static/js/runtime-main.7d381d44.js"
+  },
+  {
+    "revision": "b3b46f3d413771235ce55fdae5858c2d",
+    "url": "/weather-checker/static/media/berlin.b3b46f3d.png"
+  },
+  {
+    "revision": "ebf250b75a713cbc198171fff99e685f",
+    "url": "/weather-checker/static/media/london.ebf250b7.png"
+  },
+  {
+    "revision": "2b5e3c699dd5bf3292b5fdce9c61963b",
+    "url": "/weather-checker/static/media/madrid.2b5e3c69.png"
+  },
+  {
+    "revision": "cb91bd25b98687274c7599633ce1ec77",
+    "url": "/weather-checker/static/media/newyork.cb91bd25.png"
+  },
+  {
+    "revision": "4d5ea4dacea6e4551e1d1ce5c65a5eaf",
+    "url": "/weather-checker/static/media/paris.4d5ea4da.png"
+  },
+  {
+    "revision": "176d34b6a93d02174db54353b32e7053",
+    "url": "/weather-checker/static/media/sydney.176d34b6.png"
+  }
+]);
