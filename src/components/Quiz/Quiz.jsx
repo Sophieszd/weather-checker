0172ef049e1b2bfe1e4f77d381d2678ecc560013
Sophie <sophieszdixon@gmail.com>
2020-05-14 23:33:17 +0100

    quiz set up

---
+++ src/components/Quiz/Quiz.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import styles from "./Quiz.module.scss";
+
+export default class Quiz extends Component {
+    render() {
+        return (
+            <section></section>
+        )
+    }
+}
